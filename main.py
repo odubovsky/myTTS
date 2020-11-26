0e5e6c1a86b4ac6536a76247d1637da4e93f0bec
odeddubovsky <oded.dubovsky@gmail.com>
2020-11-26 20:04:37 +0200

    - Checks OS version - Supports Windows TTS - Looks for file on your Desktop under home dir - Minor bug fixes

--- main.py
+++ main.py
@@ -1,17 +1,34 @@
 import os
+import pyttsx3
 import random
+from sys import platform
+from pathlib import Path
 
 class randomReader:
     def __init__(self, inputFile):
         self.words = [line.strip() for line in open(inputFile, 'r')]
         self.testArr = {}
         self.testGrade = {}
+        self.engine = pyttsx3.init()
+
+        # Alternate voices
+        #voices = self.engine.getProperty('voices')
+        #self.engine.setProperty('voice', voices[1].id)
+
+    def tts(self, text):
+        if (platform == "darwin"):
+            # iOS
+            os.system("say " + text)
+        else:
+            # Windows
+            self.engine.say(text)
+            self.engine.runAndWait()
 
     def welcome(self, name):
-        os.system("say 'hello '" + name)
-        os.system("say 'How are you?'")
-        os.system("say '...Are you ready for your test?'")
-        os.system("Say 'Lets begin!...'")
+        self.tts('hello ' + name)
+        self.tts('How are you?')
+        self.tts('...Are you ready for your test?')
+        self.tts('Lets begin!...')
 
     def printWords(self, source):
         print("Total number of words: " + str(len(self.words)))
@@ -25,11 +42,10 @@ class randomReader:
     def sayWords(self, source):
         if (source == "file"):
             for w in self.words:
-                self.engine.say(w)
+                self.tts(w)
         if (source == "test"):
             for w in range(len(self.testArr)):
-                self.engine.say(self.testArr[w])
-        self.engine.runAndWait()
+                self.tts(self.testArr[w])
 
     def randomiser (self):
         index = 0
@@ -66,40 +82,44 @@ class randomReader:
         if reset == "reset":
             self.resetGrades()
         else:
-            os.system("say 'Lets try again'")
+            self.tts('Lets try again')
 
         wc = 0
         for w in range(len(self.testArr)):
             wc += 1
             # Only test words that were spelled incorrectly
             if (self.testGrade[self.testArr[w]] == 0):
-                os.system("say ''" + self.testArr[w])
+                self.tts('' + self.testArr[w])
                 answer = input("[Word " + str(wc) + "] Please type your answer: ")
                 if (answer.lower() == self.testArr[w].lower()):
                     print ("Awesome! that correct :)")
-                    os.system("say 'Thats right!'")
-                    os.system("say '...Lets move on to the next word'")
+                    self.tts('Thats right!')
                     self.testGrade[self.testArr[w]] = 1
                 else:
                     print ("That's wrong :( Here's how it should be spelled:")
-                    os.system("say 'Oooops...that was wrong'")
-                    os.system("say ' '" + self.testArr[w] + " is spelled:")
+                    self.tts('Oooops...that was wrong')
+                    self.tts(' ' + self.testArr[w] + ' is spelled:')
                     for char in range(len(self.testArr[w])):
-                        os.system("say ''" + self.testArr[w][char])
-                    os.system("say '...Lets move on to the next word'")
+                        if (self.testArr[w][char] == " "):
+                            self.tts("space")
+                        else:
+                            self.tts('' + self.testArr[w][char])
+
+                if (w < len(self.testArr) - 1):
+                    self.tts('...Lets move on to the next word')
 
         grade = self.giveGrade()
         print ("Your grade is " + str(int(grade)))
 
-        os.system("say 'Your grade is '" + str(int(grade)))
+        self.tts('Your grade is ' + str(int(grade)))
         return grade
 
 if (__name__ == "__main__"):
-    rr = randomReader ("/Users/odeddubovsky/Desktop/words.txt")
+    rr = randomReader(str(Path.home()) + "/Desktop/words.txt")
     rr.welcome("Jonathan")
 
     grade = rr.test("reset")
     while (int(grade) < 100):
         grade = rr.test("continue")
 
-    os.system("say 'Well done'")
+    rr.tts('Well done')
