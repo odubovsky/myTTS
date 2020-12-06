c65a5d57660b23a3917a0f2f328e0a53894aeda9
odeddubovsky <oded.dubovsky@gmail.com>
2020-12-06 16:38:52 +0200

    Python conventions update

--- main.py
+++ main.py
@@ -4,16 +4,17 @@ import random
 from sys import platform
 from pathlib import Path
 
-class randomReader:
-    def __init__(self, inputFile):
-        self.words = [line.strip() for line in open(inputFile, 'r')]
+
+class RandomReader:
+    def __init__(self, inputfile):
+        self.words = [line.strip() for line in open(inputfile, 'r')]
         self.testArr = {}
         self.testGrade = {}
         self.engine = pyttsx3.init()
 
         # Alternate voices
-        #voices = self.engine.getProperty('voices')
-        #self.engine.setProperty('voice', voices[1].id)
+        # voices = self.engine.getProperty('voices')
+        # self.engine.setProperty('voice', voices[1].id)
 
     def tts(self, text):
         if platform == "darwin":
@@ -27,57 +28,57 @@ class randomReader:
     def welcome(self, name):
         self.tts('hello ' + name + '. How are you?' + '...Are you ready for your test?' + 'Lets begin!...')
 
-    def printWords(self, source):
+    def printwords(self, source):
         print("Total number of words: " + str(len(self.words)))
-        if (source == "file"):
+        if source == "file":
             for w in self.words:
-                print (w)
-        if (source == "test"):
+                print(w)
+        if source == "test":
             for w in range(len(self.testArr)):
-                print (self.testArr[w])
+                print(self.testArr[w])
 
-    def sayWords(self, source):
-        if (source == "file"):
+    def saywords(self, source):
+        if source == "file":
             for w in self.words:
                 self.tts(w)
-        if (source == "test"):
+        if source == "test":
             for w in range(len(self.testArr)):
                 self.tts(self.testArr[w])
 
-    def randomiser (self):
+    def randomizer(self):
         index = 0
-        testOrder = {}
+        testorder = {}
 
-        while len(testOrder) < len(self.words):
+        while len(testorder) < len(self.words):
             # Get a random number between 0 and number of words
             word = random.randint(0, len(self.words)-1)
 
             # Continue if key already set
-            if (testOrder.get(word, "exists") != "exists"):
+            if testorder.get(word, "exists") != "exists":
                 continue
 
-            testOrder[word] = index
+            testorder[word] = index
             index += 1
 
-        for pos in testOrder:
-            self.testArr[testOrder[pos]] = self.words[pos]
+        for pos in testorder:
+            self.testArr[testorder[pos]] = self.words[pos]
 
-    def resetGrades(self):
+    def resetgrades(self):
         for w in self.words:
             self.testGrade[w] = 0
 
-    def giveGrade(self):
-        correctAnswers = 0
+    def givegrade(self):
+        correctanswers = 0
         for w in range(len(self.testArr)):
-            if (self.testGrade[self.testArr[w]] == 1):
-                correctAnswers += 1
-        return (100 * correctAnswers / len(self.testArr))
+            if self.testGrade[self.testArr[w]] == 1:
+                correctanswers += 1
+        return 100 * correctanswers / len(self.testArr)
 
     def test(self, reset):
-        self.randomiser()
+        self.randomizer()
         # Reset grades if requested
         if reset == "reset":
-            self.resetGrades()
+            self.resetgrades()
         else:
             self.tts('Lets try again')
 
@@ -85,40 +86,41 @@ class randomReader:
         for w in range(len(self.testArr)):
             wc += 1
             # Only test words that were spelled incorrectly
-            if (self.testGrade[self.testArr[w]] == 0):
-                print ("[Word " + str(wc) + "] Please type your answer: ")
+            if self.testGrade[self.testArr[w]] == 0:
+                print("[Word " + str(wc) + "] Please type your answer: ")
                 self.tts('' + self.testArr[w])
-                answer = input ("")
+                answer = input("")
                 # answer = input("[Word " + str(wc) + "] Please type your answer: ")
-                if (answer.lower() == self.testArr[w].lower()):
-                    print ("Awesome! that correct :)")
+                if answer.lower() == self.testArr[w].lower():
+                    print("Awesome! that correct :)")
                     self.tts('Thats right!')
                     self.testGrade[self.testArr[w]] = 1
                 else:
-                    print ("That's wrong :( Here's how it should be spelled:")
+                    print("That's wrong :( Here's how it should be spelled:")
                     self.tts('Oooops...that was wrong')
                     self.tts(' ' + self.testArr[w] + ' is spelled:')
                     for char in range(len(self.testArr[w])):
-                        if (self.testArr[w][char] == " "):
+                        if self.testArr[w][char] == " ":
                             self.tts("space")
                         else:
                             self.tts('' + self.testArr[w][char])
 
-                if (w < len(self.testArr) - 1):
+                if w < len(self.testArr) - 1:
                     self.tts('...Lets move on to the next word')
 
-        grade = self.giveGrade()
-        print ("Your grade is " + str(int(grade)))
+        gradecalc = self.givegrade()
+        print("Your grade is " + str(int(gradecalc)))
 
-        self.tts('Your grade is ' + str(int(grade)))
-        return grade
+        self.tts('Your grade is ' + str(int(gradecalc)))
+        return gradecalc
 
-if (__name__ == "__main__"):
-    rr = randomReader(str(Path.home()) + "/Desktop/words.txt")
+
+if __name__ == "__main__":
+    rr = RandomReader(str(Path.home()) + "/Desktop/words.txt")
     rr.welcome("Jonathan")
 
     grade = rr.test("reset")
-    while (int(grade) < 100):
+    while int(grade) < 100:
         grade = rr.test("continue")
 
     rr.tts('Well done')
